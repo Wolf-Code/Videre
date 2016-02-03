75463b7c0a368a49e84be86a3a51f3cf56af1580
Kevin <kevin.deelen@gmail.com>
2016-02-03 02:34:59 +0100

    Fixed crash when stopping the server before it has been started.

--- Videre/VidereLib/Components/NetworkComponent.cs
+++ Videre/VidereLib/Components/NetworkComponent.cs
@@ -108,6 +108,9 @@ namespace VidereLib.Components
         /// </summary>
         public void ShutdownServer( )
         {
+            if ( server == null )
+                return;
+
             server.Stop( );
             server = null;
         }
